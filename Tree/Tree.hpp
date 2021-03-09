9d04783611f4676c630a6dd9ee09552539d2d83a
BiscuitsLayer <vinogradov.so@phystech.edu>
2021-03-09 22:11:28 +0300

    New methods and bugs added

--- Tree/Tree.hpp
+++ Tree/Tree.hpp
@@ -9,6 +9,7 @@
 
 namespace AVL {
     struct Node final {
+        Node* parent_ = nullptr;
         char key_ {};
         int height_ = 1;
         int balanceFactor_ = 0;
@@ -17,9 +18,11 @@ namespace AVL {
         Node* right_ = nullptr;
 
         Node (char key):
+            parent_ (nullptr),
             key_ (key),
             height_ (1),
             balanceFactor_ (0),
+            id_ (-1),
             left_ (nullptr),
             right_ (nullptr)
             {}
@@ -30,99 +33,66 @@ namespace AVL {
             balanceFactor_ = rightHeight - leftHeight;
             height_ = std::max (leftHeight, rightHeight) + 1;
         }
-    };
-
-    class Iterator final {
-
-    };
+    }; 
 
     class Tree final {
         private:
-            Node* head_ = nullptr;
-            std::vector <Node*> nodes_ {};
+            Node* Head () {
+                if (nodes_.empty ()) {
+                    return nullptr;
+                }
+                Node* ans = &nodes_[0];
+                while (ans->parent_) {
+                    ans = ans->parent_;
+                }
+            }
+            std::vector <Node> nodes_ {};
+            
+            //  SWAP
+            void Swap (Tree& rhs) {
+                this->nodes_ = rhs.nodes_;
+            }
 
             //  INSERTION
             Node* InsertRecursive (char key, Node* node) {
                 if (!node) {
-                    node = new Node (key);
-                    node->id_ = nodes_.size ();
-                    nodes_.push_back (node);
+                    nodes_.push_back ({ key });
+                    nodes_.back ().id_ = nodes_.size () - 1;
+                    node = &nodes_.back ();    
                     return node;
                 }
                 if (key < node->key_) {
                     node->left_ = InsertRecursive (key, node->left_);
+                    node->left_->parent_ = node;
                 }
                 else {
                     node->right_ = InsertRecursive (key, node->right_);
+                    node->right_->parent_ = node;
                 }
                 return Balance (node);
-            }
-
-            //  DOT IMAGE
-            void MakeDotRecursive (std::ofstream* outfile, Node* node) {
-                if (!node) { return; }
-                *outfile << node->id_ << "[label = \"" << node->key_ << "\"]" << std::endl;
-                if (node->left_) {                    
-                    *outfile << node->left_->id_ << "[label = \"" << node->left_->key_ << "\"]" << std::endl;
-                    *outfile << node->id_ << " -> " << node->left_->id_ << std::endl;
-                    MakeDotRecursive (outfile, node->left_);
-                }
-                if (node->right_) {
-                    *outfile << node->right_->id_ << "[label = \"" << node->right_->key_ << "\"]" << std::endl;
-                    *outfile << node->id_ << " -> " << node->right_->id_ << std::endl;
-                    MakeDotRecursive (outfile, node->right_);
-                }
-            }
-
-        public:
-            friend Iterator;
-
-            //  CTORS
-            Tree ():
-                head_ (nullptr)
-                {}  
-            Tree (std::vector <char>& data) {
-                for (auto&& item : data) {
-                    Insert (item);
-                }
-            }
-
-            //  COPY
-            Tree (const Tree& rhs) = delete;
-            Tree& operator = (Tree& rhs) = delete;
-
-            //  MOVE
-            Tree (Tree&& rhs) = delete;
-            Tree& operator = (Tree&& rhs) = delete;
-
-            //  DTOR
-            ~Tree () {
-                for (auto&& node : nodes_) {
-                    delete node;
-                }
-            }
-
-            //  INSERTION AND DELETION
-            Node* Insert (char key) {
-                head_ = InsertRecursive (key, head_);
-                return head_;
-            }
-            void Delete (Iterator nodeIt) {
-
             }
 
             //  ROTATIONS
             Node* RotateLeft (Node* node) {
                 Node* temp = node->right_;
+                temp->parent_ = node->parent_;
+                node->parent_ = temp;
+                
                 node->right_ = temp->left_;
+                temp->left_->parent_ = node;
                 temp->left_ = node;
+                
                 node->Update ();
                 temp->Update ();
                 return temp;
             }
             Node* RotateRight (Node* node) {
                 Node* temp = node->left_;
+                temp->parent_ = node->parent_;
+                node->parent_ = temp;
+
                 node->left_ = temp->right_;
+                temp->right_->parent_ = node;
                 temp->right_ = node;
                 node->Update ();
                 temp->Update ();
@@ -148,9 +118,89 @@ namespace AVL {
             }
 
             //  DOT IMAGE
+            void MakeDotRecursive (std::ofstream* outfile, Node* node) {
+                if (!node) { return; }
+                *outfile << node->id_ << "[label = \"" << node->key_ << "\"]" << std::endl;
+                if (node->left_) {                    
+                    *outfile << node->left_->id_ << "[label = \"" << node->left_->key_ << "\"]" << std::endl;
+                    *outfile << node->id_ << " -> " << node->left_->id_ << std::endl;
+                    MakeDotRecursive (outfile, node->left_);
+                }
+                if (node->right_) {
+                    *outfile << node->right_->id_ << "[label = \"" << node->right_->key_ << "\"]" << std::endl;
+                    *outfile << node->id_ << " -> " << node->right_->id_ << std::endl;
+                    MakeDotRecursive (outfile, node->right_);
+                }
+            }
+
+        public:
+            class Iterator final {
+                private:
+
+                public:
+
+            };
+
+            //  CTORS
+            Tree ():
+                nodes_ ({})
+                {}  
+            Tree (std::vector <char>& data):
+                nodes_ ({})
+                {
+                    for (auto&& item : data) {
+                        Insert (item);
+                    }
+                }
+
+            //  COPY
+            Tree (const Tree& rhs):
+                nodes_ (rhs.nodes_)
+                {
+                    std::cerr << "Copy ctor" << std::endl;
+                    for (auto&& node : nodes_) {
+                        if (node.left_) {
+                            node.left_ = &nodes_[node.left_->id_];
+                        }
+                        if (node.right_) {
+                            node.right_ = &nodes_[node.right_->id_];
+                        }
+                    }
+                }
+            Tree& operator = (Tree& rhs) {
+                std::cerr << "Copy assign" << std::endl;
+                Tree temp { rhs };
+                Swap (rhs);
+                return *this;
+            }
+
+            //  MOVE
+            Tree (Tree&& rhs) = delete;
+            Tree& operator = (Tree&& rhs) = delete;
+
+            //  MODIFIERS
+            void Clear () { nodes_.clear (); }
+            Node* Insert (char key) {
+                Node* result = InsertRecursive (key, Head ());
+                return result;
+            }
+            void Extract (char key) {
+
+            }
+
+
+            //  CAPACITY
+            size_t Size () {
+                return nodes_.size ();
+            }
+            bool Empty () {
+                return nodes_.size ();
+            }
+
+            //  DOT IMAGE
             void MakeDot (std::ofstream* outfile) {
                 *outfile << "digraph G {" << std::endl << "fontsize = 50" << std::endl;
-                MakeDotRecursive (outfile, head_);
+                MakeDotRecursive (outfile, Head ());
                 *outfile << "}";
             }
     };
--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
 #include <fstream>
 
 #include "Tree/Tree.hpp"
+#include <set>
+
+void MakeDot (AVL::Tree tree) {
+	std::ofstream treeDot  { "Images/Tree.dot", std::ios::trunc };
+	if (!treeDot.is_open ()) {
+		std::cerr << "Error opening dot file!" << std::endl;
+	}
+	tree.MakeDot (&treeDot);
+}
 
 int main () {
-	AVL::Tree temp {};
+	AVL::Tree tree {};
 
-	temp.Insert ('b');
-	temp.Insert ('a');
-	temp.Insert ('q');
-	temp.Insert ('d');
-	temp.Insert ('c');
+	tree.Insert ('b');
+	tree.Insert ('a');
+	tree.Insert ('q');
+	tree.Insert ('d');
+	tree.Insert ('c');
 
 	std::ofstream treeDot  { "Images/Tree.dot", std::ios::trunc };
 	if (!treeDot.is_open ()) {
 		std::cerr << "Error opening dot file!" << std::endl;
 	}
-	temp.MakeDot (&treeDot);
+	tree.MakeDot (&treeDot);
+	//MakeDot (temp);
 	return 0;
 }
